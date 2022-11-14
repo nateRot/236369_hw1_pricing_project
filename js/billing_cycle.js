b440010d1297215d5c85d5689ab01079e56e570c
Ariel Avital <avitalariel7@gmail.com>
2022-11-14 13:36:49 +0200

    modifying billing cycle js

--- js/billing_cycle.js
+++ js/billing_cycle.js
@@ -1,32 +1,46 @@
 import { plansList } from './data.js'
 
-const params = new URLSearchParams(window.location.search)
-let value = params.get('plan_num')
-console.log(value)
+const plan_index = new URLSearchParams(window.location.search).get('plan_num')
+// let value = params.get('plan_num')
+console.log(plan_index)
+//
+// const newHeader = document.getElementById('new-header')
+// newHeader.innerHTML = 'You chose plan ' + (parseInt(value, 10) + 1)
+// const centerBox = document.getElementById('my-box')
+//
+// const createMonthlycont = () => {
+//     const monthlyButton = document.getElementById('monthly-button')
+//     monthlyButton.innerHTML =
+//         'Monthly - ' + plansList[value].monthlySubPrice + '$'
+//
+//     monthlyButton.onclick = () => {
+//         window.location.href = './checkout_form.html'
+//     }
+// }
+//
+// const createYearlycont = () => {
+//     const yearlyButton = document.getElementById('yearly-button')
+//     yearlyButton.innerHTML =
+//         'Yearly - ' + plansList[value].yearlySubPrice + '$/month'
+//
+//     yearlyButton.onclick = () => {
+//         window.location.href = './checkout_form.html'
+//     }
+// }
+//
+// createMonthlycont()
+// createYearlycont()
 
-const newHeader = document.getElementById('new-header')
-newHeader.innerHTML = 'You chose plan ' + (parseInt(value, 10) + 1)
-const centerBox = document.getElementById('my-box')
+const fetchPaymentCycleData = (index) => ({
+  "monthly": plansList[index].monthlySubPrice,
+  "yearly": plansList[index].yearlySubPrice
+});
 
-const createMonthlycont = () => {
-    const monthlyButton = document.getElementById('monthly-button')
-    monthlyButton.innerHTML =
-        'Monthly - ' + plansList[value].monthlySubPrice + '$'
+export const fillPricingCycle = (index) => {
+  const paymentCycleData = fetchPaymentCycleData(index);
+  document.getElementById("payment-cycle-monthly").innerHTML=paymentCycleData.monthly.toString() + "$";
+  document.getElementById("payment-cycle-yearly").innerHTML=paymentCycleData.yearly.toString() + "$";
+};
 
-    monthlyButton.onclick = () => {
-        window.location.href = './checkout_form.html'
-    }
-}
+fillPricingCycle(plan_index);
 
-const createYearlycont = () => {
-    const yearlyButton = document.getElementById('yearly-button')
-    yearlyButton.innerHTML =
-        'Yearly - ' + plansList[value].yearlySubPrice + '$/month'
-
-    yearlyButton.onclick = () => {
-        window.location.href = './checkout_form.html'
-    }
-}
-
-createMonthlycont()
-createYearlycont()
